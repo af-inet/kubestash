2870b1419554cdef0f28f0a582c97333b6864d90
af-inet <davidmhargat@gmail.com>
2017-08-07 14:40:06 -0400

    Better SSLError handling, proxy configuration

--- kubestash/__init__.py
+++ kubestash/__init__.py
@@ -3,6 +3,9 @@ import base64
 import json
 import sys
 
+import urllib3
+import ssl
+
 import kubernetes
 import credstash
 
@@ -29,10 +32,19 @@ def parse_args():
             action='store',
             type=str,
             help='name of the kubernetes secret you want to store secrets in')
+    parser.add_argument('-p', '--proxy',
+            action='store',
+            type=str,
+            nargs=1,
+            help='hostname of a kubernetes apiserver to use, for example: --proxy 127.0.0.1:8080')
     parser.add_argument('-v', '--verbose',
             dest='verbose',
             action='store_true',
             help='verbose output')
+    parser.add_argument('--trace',
+            dest='trace',
+            action='store_true',
+            help="show the full stack trace when an SSLError happens")
     parser.add_argument('-f', '--force',
             dest='force',
             action='store_true',
@@ -104,16 +116,43 @@ def kube_secret_exists(args, kube):
         if e.status == 404:
             return False # 404 means the secret did not exist, so we can return False
         else:
-            raise e # don't catch errors you can't resolve.
+            raise # don't catch errors you can't resolve.
+    except urllib3.exceptions.MaxRetryError as e:
+        if (type(e.reason) is urllib3.exceptions.SSLError) and not (args.trace):
+            # This will be a very common error since the python that ships with macOS
+            # seems to be stuck on openssl v0.9.8, so lets show the users how to fix it.
+            # Kubernetes seems to be aware of this issue: https://github.com/kubernetes-incubator/client-python#sslerror-on-macos
+            #
+            print(("\nSSLError: run with --trace to see the original exception which caused this error.\n\n"
+                   "This version of python is compiled with '{ssl_version}' - while Kubernetes requires at least version 1.0.0!\n\n"
+                   "You can fix this by running:\n\n"
+                   "\tkubectl proxy -p 8080\n\n"
+                   "\tkubestash --proxy 127.0.0.1:8080 {table} {secret}\n\n"
+                   "Which will subvert the issue by connecting to Kubernetes through an http proxy.\n\n"
+                   "Alternatively, you can upgrade your openssl and rebuild python3 with brew.\n\n"
+                   "\tbrew update\n"
+                   "\tbrew install openssl\n"
+                   "\tbrew uninstall python3\n"
+                   "\tbrew install python3 --with-brewed-openssl\n")
+                .format(ssl_version=ssl.OPENSSL_VERSION, table=args.table, secret=args.secret))
+            sys.exit(1)
+        else:
+            raise
     return True
 
 def main():
     args = parse_args()
 
+    # load the users config file
     kubernetes.config.load_kube_config()
+
+    # override the host if the user passes in a --proxy
+    if len(args.proxy) == 1:
+        kubernetes.client.configuration.host = args.proxy[0]
+
     kube = kubernetes.client.CoreV1Api()
     session_params = credstash.get_session_params(None, None)
-    
+
     if kube_secret_exists(args, kube):
         if not args.force:
             print("Secret: '{secret}' already exists, run with -f to replace it." \
