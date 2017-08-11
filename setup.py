1d655db4aa410dea64c0789426f5eb4d1efb005b
af-inet <davidmhargat@gmail.com>
2017-08-11 18:58:52 -0400

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     'credstash',
     'urllib3',
   ],
-  version='0.9.2',
+  version='0.9.3',
   description='Push your Credstash secrets to Kubernetes.',
   author='David Hargat',
   author_email='davidmhargat@gmail.com',
