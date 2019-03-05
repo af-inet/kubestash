3c3472def5be6db62d1b95f883a0148f7e0909d5
rapatel0 <rapatel0@gmail.com>
2019-03-05 14:39:59 +0000

    Fixed error with region
    Fixed error with the region not being detected from the arguement

--- kubestash/__init__.py
+++ kubestash/__init__.py
@@ -562,7 +562,7 @@ def filter_secrets(secrets, ns, secret):
 
 
 def get_stream_client(args):
-    client = boto3.client('dynamodbstreams')
+    client = boto3.client('dynamodbstreams', region_name=args.region)
 
     response = client.list_streams(TableName=args.table, Limit=100)
 
