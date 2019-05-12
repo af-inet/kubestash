01816411649c4de0224dc59a337f1f654e2439ef
af-inet <davidmhargat@gmail.com>
2019-05-11 23:51:01 -0400

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python setup.py sdist upload -r kubestash
+VERSION=$(python setup.py --version)
+docker build . -t "afinet/kubestash:${VERSION}"
+docker push "afinet/kubestash:${VERSION}"
